0808e59db4fae6c8a30ad730c3df0f374315677f
AlizaJawwad1719 <159164016+AlizaJawwad1719@users.noreply.github.com>
2024-11-01 17:21:03 +0500

    Add files via upload

---
+++ about us.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const aboutus = () => {
+  return (
+    <div>
+      <h1 className='flex text-gray-600 bg-blue-500 justify-center'> About Me </h1>
+      <h2 className='flex text-lg text-center p-6'> I was a Medical Student.</h2>
+      <h2 className='flex text-lg text-center p-6'> I am a good timekeeper, always willing to learn new skills. </h2>
+      <h2 className='flex text-lg text-center p-6'> I am a web developer. </h2>
+      <h2 className='flex text-lg text-center p-6'> And i am also a housewife. </h2>
+      <h2 className='flex text-lg text-center p-6'> I want to be a doctor. </h2>
+    </div>
+  )
+}
+
+export default aboutus
